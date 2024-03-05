38f3e68db88557698cc1fd317d7d8cfcdeaed2d8
Jijo Joseph <jijoputhiyath@gmail.com>
2024-03-05 17:01:22 +0400

    Added starter code

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            selectedVariantIndex: 0,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' , quantity: 50},
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' , quantity: 0 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -16,16 +16,19 @@ const app = Vue.createApp({
         addToCart() {
             this.cart += 1
         },
-        updateVariant(variantIndex) {
-            this.selectedVariantIndex = variantIndex
+        updateVariant(index) {
+            this.selectedVariant = index
         }
     },
     computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
         image() {
-            return this.variants[this.selectedVariantIndex].image
+            return this.variants[this.selectedVariant].image
         },
         inStock() {
-            return this.variants[this.selectedVariantIndex].quantity
-        }
+            return this.variants[this.selectedVariant].quantity
+        },
     }
 })
